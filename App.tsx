4e7e214918dbe2e204ab5153e29e05a00f355c6a
dev.pereira2019@gmail.com <dev.pereira2019@gmail.com>
2025-09-27 21:06:33 -0300

    feat:add

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './navigation/AppNavigator';
-import { initializeDatabase } from './database/initDB';
+import AppNavigator from './src/navigation/AppNavigator';
+import { initializeDatabase } from './src/database/initDB';
 
 const App = () => {
   useEffect(() => {
